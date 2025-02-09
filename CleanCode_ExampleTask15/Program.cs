c61bfcd250a989de12c56dcfc9799cf362a85636
Olsney <104034829+Olsney@users.noreply.github.com>
2025-02-10 00:28:46 +0200

    Added example task 15

---
+++ CleanCode_ExampleTask15/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CleanCode_ExampleTask15;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+    
+    class PlayerInfo { }
+    class Turret { }
+    class TargetFollower { }
+    class Unit { }
+    class UnitsContainer
+    {
+        public IReadOnlyCollection<Unit> Units { get; private set; }
+    }
+}
