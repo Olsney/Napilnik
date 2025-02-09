e18bb19cc0c3598e310d500efe2e50caf067b481
Olsney <104034829+Olsney@users.noreply.github.com>
2025-02-10 00:19:44 +0200

    Added example task 2

---
+++ CleanCode_ExampleTask2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CleanCode_ExampleTask2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+    
+    public static int Find(int[] array, int element)
+    {
+        for (int i = 0; i < array.Length; i++)
+            if (array[i] == element)
+                return i;
+
+        return -1;
+    }
+}
