e4f55c86914767d6fe5c60aa63d09f5323565447
Olsney <104034829+Olsney@users.noreply.github.com>
2025-02-10 00:25:24 +0200

    Added example task 11

---
+++ CleanCode_ExampleTask11/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanCode_ExampleTask11;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int armySize = 10;
+        int coins = 10;
+        string name = "Vladislav";
+    }
+}
