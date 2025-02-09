c82c00faa082071251c42bda65e40c6914968ba6
Olsney <104034829+Olsney@users.noreply.github.com>
2025-02-09 22:30:56 +0200

    Added PaymantSystems project

---
+++ PaymentSystems/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace PaymentSystems;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Выведите платёжные ссылки для трёх разных систем платежа: 
+        //pay.system1.ru/order?amount=12000RUB&hash={MD5 хеш ID заказа}
+        //order.system2.ru/pay?hash={MD5 хеш ID заказа + сумма заказа}
+        //system3.com/pay?amount=12000&curency=RUB&hash={SHA-1 хеш сумма заказа + ID заказа + секретный ключ от системы}
+    }
+}
+
+public class Order
+{
+    public readonly int Id;
+    public readonly int Amount;
+
+    public Order(int id, int amount) => (Id, Amount) = (id, amount);
+}
+
+public interface IPaymentSystem
+{
+    public string GetPayingLink(Order order);
+}
+
+public interface IHashService
+{
+    void Hash();
+}
+
+public class MD5Service : IHashService
+{
+    public void Hash()
+    {
+    }
+}
+
+public class SHAService : IHashService
+{
+    public void Hash()
+    {
+    }
+}
