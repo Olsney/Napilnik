0e8824cde4beb82671641670ea18442607b00f31
Olsney <104034829+Olsney@users.noreply.github.com>
2025-02-09 23:53:58 +0200

    Add ExampleTask1

---
+++ CleanCode_ExampleTask1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace CleanCode_ExampleTask1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+    
+    public static int Clamp(int value, int min, int max)
+    {
+        if (value < min)
+            return min;
+        else if (value > max)
+            return max;
+        else
+            return value;
+    }
+}
