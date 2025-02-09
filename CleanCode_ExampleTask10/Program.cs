8c18bb5d287948cc72e73a68b73071c09cc2d2ae
Olsney <104034829+Olsney@users.noreply.github.com>
2025-02-10 00:24:08 +0200

    Added example task 10

---
+++ CleanCode_ExampleTask10/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace CleanCode_ExampleTask10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+    
+    class Weapon
+    {
+        private int _bulletsAmount;
+        private int _bulletsPerShot = 1;
+
+        public bool CanShoot => _bulletsAmount > _bulletsPerShot;
+
+        public void Shoot()
+        {
+            if (CanShoot == false)
+                throw new InvalidOperationException("Not enough bullets.");
+
+            _bulletsAmount -= _bulletsPerShot;
+        }
+    }
+}
